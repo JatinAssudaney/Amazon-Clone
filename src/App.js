47897d0ad2c15af00066f05cdba1e0f5189cdd2e
JatinAssudaney <jatin.assudaney24@gmail.com>
2020-10-26 14:37:39 +0530

    Duplicated from ReactAppClones Repo

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import { auth } from "./firebase";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Checkout from "./Checkout";
+import Header from "./Header";
+import Home from "./Home";
+import Login from "./Login";
+import { useStateValue } from "./StateProvider";
+import Payment from "./Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import Orders from "./Orders";
+
+const promise = loadStripe(
+  "pk_test_51HQcnjLid9ObrHDm46A4rZocXgRgU2l1sDlATGJSqcZJXoQSm1Qvitow63I8y3p4MtLXV7ABJ4VywYcwEjBfg42C00OviznPwf"
+);
+
+function App() {
+  const [{ user }, dispatch] = useStateValue();
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch({ type: "SET_USER", user: authUser });
+      } else {
+        dispatch({ type: "SET_USER", user: null });
+      }
+    });
+  }, []);
+
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
